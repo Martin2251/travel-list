2aed4710c9819d911036da7cd67205c5ba6f6518
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-08-06 11:36:02 +0100

    updating onchange

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -28,6 +29,9 @@ export default function App() {
 
  export const Form = () => {
 
+  const [description, setDescription] = useState("");
+  const [quantity,setQuantity] = useState(1)
+
   function handleSubmit (e) {
     e.preventDefault();
 
@@ -35,10 +39,10 @@ export default function App() {
   return (
     <form className='add-form' onSubmit={handleSubmit}>
       <h3> what do you need for your trip?</h3>
-      <select>
+      <select value={quantity} onChange={(e) =>setQuantity(Number(e.target.value))}>
        {Array.from({length:20},(_,i) => i +1).map((num) =>(<option value={num} key={num}>{num}</option>))}
       </select>
-      <input type="text" placeholder="Add to packing list"/>
+      <input type="text" placeholder="Add to packing list" value={description} onChange={(e) =>setDescription(e.target.value)}/>
       <button>Add</button>
     </form>
   )
