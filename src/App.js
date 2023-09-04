e4afdb9dbd884b848ac1b528d2decebd9c9af828
Martin Lowe <martinlowe673@gmail.com>
2023-09-04 07:34:04 +0100

    new files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-
+import Logo from "./Logo";
+import Form from "./Form";
+import { PackingList } from "./PackingList";
 
 
 export default function App() {
@@ -42,75 +44,11 @@ function handleClearList(){
 
 
 
- export  const Logo = () => {
-  return (
-    <h1>🏝️ Far Away 🧳</h1>
-  )
-}
 
-
- export const Form = ({onAddItems}) => {
-
-  const [description, setDescription] = useState("");
-  const [quantity,setQuantity] = useState(1);
 
  
 
-  function handleSubmit (e) {
-    e.preventDefault();
 
-    if(!description) return;
-
-    const newItem ={description,quantity,packed:false, id:Date.now()}
-    console.log(newItem)
-
-    // with new item we have just created 
-    onAddItems(newItem)
-
-    setDescription("");
-    setQuantity(1)
-
-  }
-  return (
-    <form className='add-form' onSubmit={handleSubmit}>
-      <h3> what do you need for your trip?</h3>
-      <select value={quantity} onChange={(e) =>setQuantity(Number(e.target.value))}>
-       {Array.from({length:20},(_,i) => i +1).map((num) =>(<option value={num} key={num}>{num}</option>))}
-      </select>
-      <input type="text" placeholder="Add to packing list" value={description} onChange={(e) =>setDescription(e.target.value)}/>
-      <button>Add</button>
-    </form>
-  )
-}
-
-
-
-
-export const PackingList = ({items, onDeleteItem,onToggleItem,onClearList}) => {
-  const [sortBy, setSortBy] = useState("input");
-  
-  let sortedItems;
-  if(sortBy === "input") sortedItems = items;
-
-  if(sortBy === "description") sortedItems = items.slice().sort((a,b) =>a.description.localeCompare(b.description))
-
-  if (sortBy === "packed") sortedItems = items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
-  return (
-    <div className='list'>
-    <ul>
-      {sortedItems.map(item => <Item item={item} key={item.id} onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} />)}
-    </ul> 
-    <div className="actions">
-      <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-        <option value="input">Sort by input order</option>
-        <option value="description">Sort  by description</option>
-        <option value="packed">Sort by packed status</option>
-      </select>
-      <button onClick={onClearList}>clear list</button>
-    </div>
-    </div>
-  )
-}
 
 export const Item = ({item, onDeleteItem,onToggleItem}) =>{
   return (
